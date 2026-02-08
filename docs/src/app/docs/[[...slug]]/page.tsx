bb0f8e8bf07a280b7b987c4559d9b2e10e221dc9
Khánh Hoàng <hi@khanh.id>
2026-02-08 18:24:21 +0700

    Refactor class order in `page.tsx` for consistency and update step name in autofix workflow for clarity.

--- docs/src/app/docs/[[...slug]]/page.tsx
+++ docs/src/app/docs/[[...slug]]/page.tsx
@@ -29,7 +29,7 @@ export default async function Page(props: PageProps<'/docs/[[...slug]]'>) {
       <DocsDescription className='mb-0'>
         {page.data.description}
       </DocsDescription>
-      <div className='flex flex-row items-center gap-2 border-b pb-6'>
+      <div className='flex flex-row gap-2 items-center border-b pb-6'>
         <LLMCopyButton markdownUrl={`${page.url}.mdx`} />
         <ViewOptions
           githubUrl={`https://github.com/${gitConfig.user}/${gitConfig.repo}/blob/${gitConfig.branch}/docs/content/docs/${page.path}`}
